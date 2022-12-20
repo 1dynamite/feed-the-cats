96ab60f47cd24313e862b2abc5d3be216fbed8e6
Shohkrukh Turaev <turaevshokhrukh7@gmail.com>
2022-12-20 19:03:29 +0500

    use function components instead of class

--- src/App.tsx
+++ src/App.tsx
@@ -1,111 +1,92 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { CatType } from "./types";
 import getRandomCat from "./utils";
 import CatItem from "./cat-item";
 
-class App extends React.Component<{}, { allCats: CatType[] }> {
-  intervalId: undefined | ReturnType<typeof setInterval> = undefined;
+let intervalId: undefined | ReturnType<typeof setInterval> = undefined;
 
-  constructor(props: {}) {
-    super(props);
-    this.state = {
-      allCats: [],
-    };
-  }
+function App() {
+  const [cats, setCats] = useState<CatType[]>([]);
 
-  componentDidMount(): void {
+  useEffect(() => {
     const cb = () => {
       const newCat = getRandomCat();
 
-      this.setState((prevState) => ({
-        allCats: [...prevState.allCats, newCat],
-      }));
+      setCats((cats) => [...cats, newCat]);
 
-      this.startTimeoutForCatItem(newCat.id);
+      startTimeoutForCatItem(newCat.id);
     };
 
     cb();
 
-    this.intervalId = setInterval(cb, 5000);
-  }
+    intervalId = setInterval(cb, 5000);
 
-  componentWillUnmount(): void {
-    this.setState({ allCats: [] }); // In development mode, cb() in componentDidMount will execute twice, so two cats will be added, this can be removed in production mode
-    clearTimeout(this.intervalId);
-    this.state.allCats.forEach((el) => clearTimeout(el.timeoutId));
-  }
+    return () => {
+      setCats([]); // In development mode, cb() in componentDidMount will execute twice, so two cats will be added, this can be removed in production mode
+      clearTimeout(intervalId);
+      cats.forEach((cat) => clearTimeout(cat.timeoutId));
+    };
+  }, []);
 
-  handleFeedCat = (id: number) => {
-    clearTimeout(this.state.allCats.find((el) => el.id === id)?.timeoutId);
+  const handleFeedCat = (id: number) => {
+    clearTimeout(cats.find((el) => el.id === id)?.timeoutId);
 
-    this.setState((prevState) => ({
-      allCats: prevState.allCats.map((el) =>
-        el.id === id ? { ...el, hungry: false } : el
-      ),
-    }));
+    setCats((cats) =>
+      cats.map((cat) => (cat.id === id ? { ...cat, hungry: false } : cat))
+    );
 
-    this.startTimeoutForCatItem(id);
+    startTimeoutForCatItem(id);
   };
 
-  startTimeoutForCatItem = (id: number) => {
+  const startTimeoutForCatItem = (id: number) => {
     const timeoutId = setTimeout(() => {
       const timeoutId = setTimeout(() => {
-        this.setState((prevState) => ({
-          allCats: prevState.allCats.filter((el) => el.id !== id),
-        }));
+        setCats((cats) => cats.filter((el) => el.id !== id));
       }, 5000);
 
-      this.setState((prevState) => ({
-        allCats: prevState.allCats.map((el) =>
+      setCats((cats) =>
+        cats.map((el) =>
           el.id === id ? { ...el, hungry: true, timeoutId } : el
-        ),
-      }));
+        )
+      );
     }, 30000);
 
-    this.setState((prevState) => ({
-      allCats: prevState.allCats.map((el) =>
-        el.id === id ? { ...el, timeoutId } : el
-      ),
-    }));
+    setCats((cats) =>
+      cats.map((el) => (el.id === id ? { ...el, timeoutId } : el))
+    );
   };
 
-  render() {
-    return (
-      <div className="app">
-        <div className="cats-container">
-          <p className="heading">All Cats</p>
-          <div className="cats-list-container">
-            <div className="cats-list">
-              {this.state.allCats.map((el) => (
-                <CatItem
-                  key={el.id}
-                  {...el}
-                  handleFeedCat={this.handleFeedCat}
-                />
-              ))}
-            </div>
-          </div>
-        </div>
-        <div className="cats-container">
-          <p className="heading">Neighbour's Cats</p>
-          <div className="cats-list-container">
-            <div className="cats-list">
-              {this.state.allCats
-                .filter((el) => !el.hasCollar)
-                .map((homelessCat) => (
-                  <CatItem
-                    key={homelessCat.id}
-                    {...homelessCat}
-                    handleFeedCat={this.handleFeedCat}
-                  />
-                ))}
-            </div>
+  return (
+    <div className="app">
+      <div className="cats-container">
+        <p className="heading">All Cats</p>
+        <div className="cats-list-container">
+          <div className="cats-list">
+            {cats.map((cat) => (
+              <CatItem key={cat.id} {...cat} handleFeedCat={handleFeedCat} />
+            ))}
           </div>
         </div>
       </div>
-    );
-  }
+      <div className="cats-container">
+        <p className="heading">Neighbour's Cats</p>
+        <div className="cats-list-container">
+          <div className="cats-list">
+            {cats
+              .filter((cat) => !cat.hasCollar)
+              .map((homelessCat) => (
+                <CatItem
+                  key={homelessCat.id}
+                  {...homelessCat}
+                  handleFeedCat={handleFeedCat}
+                />
+              ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/cat-item.tsx
+++ src/cat-item.tsx
@@ -1,38 +1,32 @@
-import React from "react";
 import { CatItemPropsType } from "./types";
 
-export default class CatItem extends React.Component<CatItemPropsType, {}> {
-  render() {
-    return (
-      <div className="cat-item">
-        <div className="collar">
-          {this.props.hasCollar ? (
-            <img
-              className="collar-img"
-              alt="collar"
-              src="https://i.imgur.com/hIVdMR6.jpg"
-            />
-          ) : (
-            <span>No collar</span>
-          )}
-        </div>
-        <div className="info">
-          <span>{this.props.name}</span>
-          <span>{this.props.age}</span>
-        </div>
-        <div
-          className="cat-color"
-          style={{ backgroundColor: this.props.color }}
-        ></div>
-        {this.props.hungry ? (
-          <div
-            className="feed-me-popup"
-            onClick={() => this.props.handleFeedCat(this.props.id)}
-          >
-            Feed me
-          </div>
-        ) : null}
+export default function CatItem(props: CatItemPropsType) {
+  return (
+    <div className="cat-item">
+      <div className="collar">
+        {props.hasCollar ? (
+          <img
+            className="collar-img"
+            alt="collar"
+            src="https://i.imgur.com/hIVdMR6.jpg"
+          />
+        ) : (
+          <span>No collar</span>
+        )}
       </div>
-    );
-  }
+      <div className="info">
+        <span>{props.name}</span>
+        <span>{props.age}</span>
+      </div>
+      <div className="cat-color" style={{ backgroundColor: props.color }}></div>
+      {props.hungry ? (
+        <div
+          className="feed-me-popup"
+          onClick={() => props.handleFeedCat(props.id)}
+        >
+          Feed me
+        </div>
+      ) : null}
+    </div>
+  );
 }
